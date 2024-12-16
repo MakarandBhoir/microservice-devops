2270649364097542b66ef9734dd7922a431ca8c4
makarand.bhoir <makarand.bhoir@live.com>
2024-12-16 16:39:24 +0530

    02 microservice code

---
+++ 03-course-microservice/src/main/java/com/synechron/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.synechron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// change1
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
