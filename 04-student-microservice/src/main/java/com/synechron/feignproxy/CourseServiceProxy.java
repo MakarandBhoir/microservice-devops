b41d7b07d1e2b99149ffcb5286cb29789a0b080d
makarand.bhoir <makarand.bhoir@live.com>
2024-12-17 09:27:57 +0530

    04-services added

---
+++ 04-student-microservice/src/main/java/com/synechron/feignproxy/CourseServiceProxy.java
@@ -0,0 +1,18 @@
+package com.synechron.feignproxy;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.synechron.model.Course;
+
+//@FeignClient(name = "COURSE-MICROSERVICE", path = "/courses")
+@FeignClient(name = "course-microservice", path = "/courses")
+public interface CourseServiceProxy {
+	@GetMapping(path="student/{stuId}")
+	
+	// http://course-microservice/courses/student/1
+	public List<Course> getCoursesByStudentId(@PathVariable("stuId") int studentId);
+}
